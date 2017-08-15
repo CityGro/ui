ffbf6e767e721ddc4684e7462a834cb2601a0151
Carlos Killpack <carlos@citygro.com>
2017-08-15 13:58:12 -0600

    add Icon, ModalHeader

---
+++ Icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <i class="ui-icon fa" :class="iconClass" />
+</template>
+
+<script>
+export default {
+  name: 'ui-icon',
+  props: {
+    name: {
+      type: String,
+      default: 'fa'
+    },
+    spin: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    iconClass () {
+      return {
+        'fa-spin': this.spin,
+        [`fa-${this.name}`]: true
+      }
+    }
+  }
+}
+</script>
---
+++ ModalHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui-modal-header modal-header">
+    <a class="close" @click="$emit('close')">
+      <icon name="times" />
+    </a>
+    <h3 class="modal-title">
+      <slot />
+    </h3>
+    <slot name="extra" />
+  </div>
+</template>
+
+<script>
+import Icon from './Icon'
+
+export default {
+  name: 'ui-modal-header',
+  components: {
+    Icon
+  }
+}
+</script>
